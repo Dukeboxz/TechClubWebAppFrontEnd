77eb23c84c8152d1494d4f3a01bc2fa7fcda63cb
Dukeboxz <u04sjj@gmail.com>
2020-04-23 15:34:22 +0100

    changed url for badgeGet

--- src/app/badge.service.ts
+++ src/app/badge.service.ts
@@ -1,9 +1,37 @@
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import {catchError, map, tap} from 'rxjs/operators';
+import {Observable, of} from 'rxjs'
 import { Injectable } from '@angular/core';
+import { Badge } from './Badge';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BadgeService {
 
-  constructor() { }
+ private badgeUrl = 'http://localhost:3000/badge'
+
+  constructor(private http: HttpClient) { }
+
+  getBadges() : Observable<Badge[]> {
+
+    return this.http.get<Badge[]>(this.badgeUrl)
+  }
+
+  getBadgeFromName( badgeName: string) : Observable<Badge>{
+
+    const url = `${this.badgeUrl}/${badgeName}`;
+    return this.http.get<Badge>(url);
+  }
+
+  private handleError<T> (operation = 'operation', result?:T){
+    return (error: any): Observable<T> => {
+
+      console.log(error); 
+
+      
+
+      return of(result as T)
+    }
+  }
 }
